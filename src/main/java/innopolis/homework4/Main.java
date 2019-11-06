d23223f0419fb22d8c39779731d14604700db403
Alexey <vox613@gmail.com>
2019-11-06 13:30:44 +0300

    First commit!

---
+++ src/main/java/innopolis/homework4/Main.java
@@ -0,0 +1,12 @@
+package innopolis.homework4;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static void main( String[] args ) {
+        System.out.println( "First commit!" );
+    }
+}
